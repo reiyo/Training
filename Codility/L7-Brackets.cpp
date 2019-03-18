e1a12a0cf3b4c1fb4fc31b241065bc396c304418
reiyo <khho@eda.ee.ntu.edu.tw>
2019-03-19 01:31:00 +0800

    Brackets.

---
+++ Codility/L7-Brackets.cpp
@@ -0,0 +1,22 @@
+#include <stack>
+
+int solution(string &S) {
+    // write your code in C++14 (g++ 6.2.0)
+    std::stack<char> mystack;
+    
+    for (string::const_iterator cit=S.begin(); cit!=S.end(); ++cit) {
+        if (*cit == '{') {
+            mystack.push('}');
+        } else if (*cit == '[') {
+            mystack.push(']');
+        } else if (*cit == '(') {
+            mystack.push(')');
+        } else if (*cit == mystack.top()) {
+            mystack.pop();
+        } else {
+            return 0;
+        }
+    }
+    
+    return (mystack.empty())? 1 : 0;
+}
