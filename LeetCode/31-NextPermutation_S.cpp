d57fe0213a1845568239af22447001da7e19de7a
reiyo <47709174+reiyo@users.noreply.github.com>
2021-02-27 02:28:37 +0800

    Simplify the codes

--- LeetCode/31-NextPermutation_S.cpp
+++ LeetCode/31-NextPermutation_S.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public:    
     void nextPermutation(vector<int>& nums) {
         if (nums.size() <= 1) return;
         int t = nums.size()-2;
@@ -7,28 +7,19 @@ public:
         while (t >= 0 && nums[t] >= nums[t+1]) -- t;
         
         if (t < 0) {
-            reverseSort(0, nums.size()-1, nums);
+            reverse(nums.begin(), nums.end());
             return;
         }
-        
-        int min_id = t+1;
-        
-        for (int i=t+1; i<nums.size(); ++i) {
-            if (nums[i] <= nums[t]) continue;
-            if (nums[i] <= nums[min_id]) {
-                min_id = i;
-            }
+
+        // search for the smallest one greater than nums[t]
+        int i=t+1;
+
+        for (; i<nums.size(); ++i) {
+            if (nums[i]<=nums[t]) break;
         }
-        
-        swap(nums[t], nums[min_id]);
-        reverseSort(t+1, nums.size()-1, nums);
-    }
-    
-    void reverseSort(int start, int end, vector<int>& nums) {
-        while (start < end) {
-            swap(nums[start], nums[end]);
-            ++ start;
-            -- end;
-        }
+
+        // e.g., 125310 -> 135210 -> 130125
+        swap(nums[t], nums[i-1]);
+        reverse(nums.begin()+t+1, nums.end());
     }
 };
