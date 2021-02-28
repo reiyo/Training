f253cd77c60ee05c785cf95a85aa64b9f5c02638
reiyo <47709174+reiyo@users.noreply.github.com>
2021-02-28 17:55:26 +0800

    Update 33-SearchinRotatedSortedArray_S.cpp

--- LeetCode/33-SearchinRotatedSortedArray_S.cpp
+++ LeetCode/33-SearchinRotatedSortedArray_S.cpp
@@ -27,3 +27,33 @@ public:
         return -1;
     }
 };
+
+// same idea: see the pivot is right or left with respect to the mid 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int last = nums.size()-1;
+        int left = 0, right = last;
+        
+        while (left <= right) {
+            int mid = (left+right)/2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) {
+                if (nums[last] > nums[mid]) {
+                    if (nums[last]<target) right = mid-1;
+                    else left = mid+1;
+                }
+                else left = mid+1;
+            }
+            else {
+                if (nums[last] > nums[mid]) right = mid-1;
+                else {
+                    if (nums[last]>=target) left = mid+1;
+                    else right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
