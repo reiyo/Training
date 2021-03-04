5b6ef60eeb9fb5a9e4487c515aa16766f765eb4b
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-05 00:01:15 +0800

    Update 146-LRUCache_S.cpp

--- LeetCode/146-LRUCache_S.cpp
+++ LeetCode/146-LRUCache_S.cpp
@@ -34,3 +34,40 @@ private:
     list<pair<int, int>> l; // key, value
     int cap;
 };
+
+// almost the same, more simpler
+class LRUCache {
+public: 
+    LRUCache(int capacity) {
+        this->cap = capacity;
+    }
+    
+    int get(int key) {
+        if (mymap.count(key)) {
+            keys.erase(mymap[key].second);
+            keys.push_front(key);
+            mymap[key].second = keys.begin();
+            return mymap[key].first;
+        }
+        else return -1;
+    }
+    
+    void put(int key, int value) {
+        if (mymap.count(key)) {
+            keys.erase(mymap[key].second);
+        }
+        else if (mymap.size() >= cap) {
+            int last = keys.back();
+            mymap.erase(last);
+            keys.pop_back();   
+        }       
+
+        keys.push_front(key);
+        mymap[key] = make_pair(value, keys.begin());        
+    }
+
+private:
+    unordered_map<int, pair<int, list<int>::iterator> > mymap; // pair consists of value and index of list
+    list<int> keys;
+    int cap;
+};
