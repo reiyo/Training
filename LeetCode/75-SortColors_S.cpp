134e1335700042daac77652b34f169f06be1843d
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-08 01:41:55 +0800

    Update 75-SortColors_S.cpp

--- LeetCode/75-SortColors_S.cpp
+++ LeetCode/75-SortColors_S.cpp
@@ -12,10 +12,36 @@ public:
             } else if (nums[pivot] == 1) {
                 ++ pivot;
             } else {
+                // we don't know what nums[right] has before the swap, 
+                // so we leave pivot for the next iteration
                 swap(nums[right--], nums[pivot]);
             }
         }
         
         return;
+    }
+};
+
+// almost the same idea
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        if (nums.size() <= 1) return;
+        int r=0;
+        int b=nums.size()-1;
+        int i=0;
+
+        while (i <= b) {
+            if (nums[i] == 0) {
+                swap(nums[r++], nums[i]);
+                if (nums[i] == 2) swap(nums[b--], nums[i]);
+            }
+            else if (nums[i] == 2) {
+                swap(nums[b--], nums[i]);
+                if (nums[i] == 0) swap(nums[r++], nums[i]);
+            }
+            // the if-statement is essential, e.g., for [2, 1, 2]
+            if (nums[i] != 2) ++i;
+        }
     }
 };
