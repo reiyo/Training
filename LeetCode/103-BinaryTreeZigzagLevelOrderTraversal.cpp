252b66252d01eb3525f9bb65458d089510962d85
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-07 16:07:06 +0800

    Update 103-BinaryTreeZigzagLevelOrderTraversal.cpp

--- LeetCode/103-BinaryTreeZigzagLevelOrderTraversal.cpp
+++ LeetCode/103-BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -1,3 +1,4 @@
+// DFS
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -23,5 +24,44 @@ public:
         
         traverse(t->left, level+1, results);
         traverse(t->right, level+1, results);
+    }
+};
+
+// BFS
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int> > outs;
+        if (root == nullptr) return outs;
+        
+        queue<TreeNode*> q;
+        unordered_map<TreeNode*, int> m;
+        q.push(root);
+        m[root] = 0;
+
+        while (!q.empty()) {
+            TreeNode* ptr = q.front();
+            q.pop();
+            int level = m[ptr];
+            
+            if (outs.size() <= level) outs.push_back(vector<int>());
+            
+            outs[level].push_back(ptr->val);
+            
+            if (ptr->left != nullptr) {
+                q.push(ptr->left);
+                m[ptr->left] = level + 1;
+            }
+            if (ptr->right != nullptr) {
+                q.push(ptr->right);
+                m[ptr->right] = level + 1;
+            }
+        }
+        
+        for (int i=1; i<outs.size(); i+=2) {
+            reverse(outs[i].begin(), outs[i].end());
+        }
+        
+        return outs;
     }
 };
