4abbe2f10de9efd27ae8a86daf59a5812b9010e3
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-16 22:22:27 +0800

    Update 13-RomantoInteger.cpp

--- LeetCode/13-RomantoInteger.cpp
+++ LeetCode/13-RomantoInteger.cpp
@@ -26,3 +26,21 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> dict({{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100},
+                                       {'D', 500}, {'M', 1000}});
+        if (1 == s.length()) return dict[s[0]];
+        int res = 0;
+        int i=0;
+        for (; i+1<static_cast<int>(s.length()); ++i) {
+            if (dict[s[i]] < dict[s[i+1]]) res -= dict[s[i]];
+            else res += dict[s[i]];
+        }
+
+        res += dict[s[i]];
+        return res;
+    }
+};
