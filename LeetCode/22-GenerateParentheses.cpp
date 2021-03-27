aec8676f8cd29abc91db7c576c20185f7ddc1e85
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-28 02:25:51 +0800

    Update 22-GenerateParentheses.cpp

--- LeetCode/22-GenerateParentheses.cpp
+++ LeetCode/22-GenerateParentheses.cpp
@@ -26,3 +26,24 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generate(string(""), n, 0, 0, res);
+        return res;                
+    }
+
+    void generate(string s, int n, int left, int right, vector<string> &res) {
+        if (left >= n && right >= n) {
+            res.push_back(s);
+            return;
+        }
+
+        if (left < n) generate(s+"(", n, left+1, right, res);
+
+        //if (right < n && right < left)
+        if (right < left) generate(s+")", n, left, right+1, res); 
+    }
+};
