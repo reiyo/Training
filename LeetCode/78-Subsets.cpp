9899eb6eb65eef7050bcd11bbbe5f1e633b16a83
reiyo <47709174+reiyo@users.noreply.github.com>
2021-03-29 00:38:13 +0800

    Update 78-Subsets.cpp

--- LeetCode/78-Subsets.cpp
+++ LeetCode/78-Subsets.cpp
@@ -1,3 +1,47 @@
+// Cascading
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > res;
+        res.push_back(vector<int>());
+
+        for (int n: nums) {
+            int end = static_cast<int>(res.size());
+
+            for (int i=0; i<end; ++i) {
+                vector<int> temp = res[i];
+                temp.push_back(n);
+                res.emplace_back(temp);
+            }
+        }
+
+        return res;
+    }
+};
+
+// Bit-manipulation from https://leetcode.com/problems/subsets/discuss/27278
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size(), p = 1 << n;
+        
+        // subs.size()=8 for n=3
+        vector<vector<int>> subs(p);
+        
+        // i can be viewed as 0, 01, 10, 11, ..., 111 
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                // every two, every four, every eight, ...
+                if ((i >> j) & 1) {
+                    subs[i].push_back(nums[j]);
+                }
+            }
+        }
+        return subs;
+    }
+};
+
+// Backtracking
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
