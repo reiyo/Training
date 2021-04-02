2ad283209ebd3270433dc15a39e43f5857ab87fe
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-02 16:42:57 +0800

    Update 42-TrappingRainWater_S.cpp

--- LeetCode/42-TrappingRainWater_S.cpp
+++ LeetCode/42-TrappingRainWater_S.cpp
@@ -26,3 +26,36 @@ public:
         return sum;
     }
 };
+
+// dynamic programming: O(n) space
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = static_cast<int>(height.size());
+        if (n <= 2) return 0;
+
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+
+        left[0] = height[0];
+        for (int i=1; i<n; ++i) {
+            left[i] = max(left[i-1], height[i]);
+        }
+
+        right[n-1] = height[n-1];
+        for (int i=n-2; i>=0; --i) {
+            right[i] = max(right[i+1], height[i]);
+        }
+
+        int res=0;
+        for (int i=1; i<n-1; ++i) {
+            int minh = min(left[i], right[i]);
+
+            if (height[i] < minh) {
+                res += (minh-height[i]);
+            }
+        }
+
+        return res;
+    }
+};
