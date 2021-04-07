fab496580ab0548a22c1dcc24c0cdfedd7064e23
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-08 01:49:19 +0800

    Update 70-ClimbingStairs.cpp

--- LeetCode/70-ClimbingStairs.cpp
+++ LeetCode/70-ClimbingStairs.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 1) return 1;
-            
-        int prev_prev = 1;
-        int prev = 1;
-        int cur = 0;
+        if (n == 1) return 1;
+        else if (n == 2) return 2;
+        int first = 1;
+        int second = 2;
         
-        for (int i=2; i<=n; ++i) {
-            cur = prev + prev_prev;
-            prev_prev = prev;
-            prev = cur;
+        for (int i=3; i<=n; ++i) {
+            int ways = first + second;
+            first = second;
+            second = ways;
         }
         
-        return cur;
+        return second;
     }
 };
