edec1fe2111a02c690de87e298bbe29ceec0bd7f
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-18 03:05:03 +0800

    Create 208-ImplementTriePrefixTree.cpp

---
+++ LeetCode/208-ImplementTriePrefixTree.cpp
@@ -0,0 +1,68 @@
+class Node {
+public:
+    Node() {
+        memset(child, 0, sizeof(child));
+        // void *memset(void *s, int c, size_t n)
+        // as nullptr is not an integer, so it does not work below
+        // memset(child, nullptr, 26);
+        
+        is_leaf = false;
+    }
+    
+    Node* child[26];
+    bool is_leaf;
+};
+
+class Trie {
+public:
+    Node* root;
+    
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new Node();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Node* cur = root;
+        
+        for (char c: word) {
+            if (cur->child[c-'a'] == nullptr) cur->child[c-'a'] = new Node();
+            cur = cur->child[c-'a'];
+        }
+        
+        cur->is_leaf = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Node* cur = root;
+        
+        for (char c: word) {
+            if (cur->child[c-'a'] == nullptr) return false;
+            cur = cur->child[c-'a'];
+        }
+        
+        return cur->is_leaf;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Node* cur = root;
+        
+        for (char c: prefix) {
+            if (cur->child[c-'a'] == nullptr) return false;
+            cur = cur->child[c-'a'];
+        }
+        
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
