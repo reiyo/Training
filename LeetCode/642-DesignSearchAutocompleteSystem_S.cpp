0dd6a721fe004fa083b1ba2ad72fc21548d7b27d
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-18 22:40:40 +0800

    Update 642-DesignSearchAutocompleteSystem_S.cpp

--- LeetCode/642-DesignSearchAutocompleteSystem_S.cpp
+++ LeetCode/642-DesignSearchAutocompleteSystem_S.cpp
@@ -86,3 +86,97 @@ private:
  * AutocompleteSystem* obj = new AutocompleteSystem(sentences, times);
  * vector<string> param_1 = obj->input(c);
  */
+
+// almost the same, but using sort
+class TrieNode {
+public:
+    TrieNode(): cnt(0) {
+    }
+
+    unordered_map<char, TrieNode*> children;
+    string sent;
+    int cnt;
+};
+
+bool cmp(const pair<string, int>& p1, const pair<string, int>& p2) {
+    if (p1.second != p2.second) return p1.second > p2.second;
+    return p1.first < p2.first;
+}
+
+class AutocompleteSystem {
+public:
+    AutocompleteSystem(vector<string>& sentences, vector<int>& times) {
+        root = new TrieNode();
+
+        for (int i=0; i<sentences.size(); ++i) {
+            tree_insert(sentences[i], times[i]);
+        }
+        
+        temp = root;
+    }
+
+    void tree_insert(string& str, int cnt) {
+        TrieNode* cur = root;
+
+        for (char ch: str) {
+            if (cur->children.find(ch) == cur->children.end()) cur->children[ch] = new TrieNode();
+            cur = cur->children[ch];
+        }
+
+        cur->sent = str;
+        cur->cnt = cnt;
+    }
+
+    void tree_collect(TrieNode* cur, vector<pair<string, int> > &out) {
+        if (!cur->sent.empty()) {
+            out.push_back(make_pair(cur->sent, cur->cnt));
+        }
+
+        for (auto it=cur->children.begin(); it!=cur->children.end(); ++it) tree_collect(it->second, out); 
+    }
+    
+    vector<string> input(char c) {
+        if (c == '#') {
+            if (temp) {
+                if (temp->sent.empty()) temp->sent = prefix; // forgot to add this caused a bug
+                ++ temp->cnt;
+            }
+            else tree_insert(prefix, 1);
+
+            temp = root;
+            prefix = "";
+            return vector<string>();
+        }
+
+        prefix += c;
+        if (temp == NULL) return vector<string>();
+
+        if (temp->children.find(c) != temp->children.end()) {
+            temp = temp->children[c];
+        }
+        else {
+            temp = NULL;        
+            return vector<string>();
+        }
+
+        vector<pair<string, int> > candidates;
+        tree_collect(temp, candidates);
+        sort(candidates.begin(), candidates.end(), cmp);
+
+        vector<string> res;
+        for (int i=0; i<3 && i<candidates.size(); ++i) {
+            res.push_back(candidates[i].first);
+        }   
+        return res;
+    }
+private:
+    TrieNode* root;
+    TrieNode* temp;
+    string prefix;
+};
+
+/**
+ * Your AutocompleteSystem object will be instantiated and called as such:
+ * AutocompleteSystem* obj = new AutocompleteSystem(sentences, times);
+ * vector<string> param_1 = obj->input(c);
+ */
