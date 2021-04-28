26a2954c5e4963c0bbbe0b1a33741e6c5d47a2e2
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-28 23:55:50 +0800

    Update 121-BestTimetoBuyandSellStock.cpp

--- LeetCode/121-BestTimetoBuyandSellStock.cpp
+++ LeetCode/121-BestTimetoBuyandSellStock.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxgain = 0;
-        int buy = 0;
-        
-        for (int i=0; i<prices.size(); ++i) {
-            if (prices[i] < prices[buy]) buy = i;
-            else {
-                int diff = prices[i] - prices[buy];
-                if (diff > maxgain) maxgain = diff;
-            }
+        int curmin = prices[0];
+        int maxpro = 0;
+
+        for (int p: prices) {
+            if (p<curmin) curmin = p;
+            else if (p - curmin > maxpro) maxpro = p - curmin;            
         }
-        return maxgain;
+
+        return maxpro;
     }
 };
