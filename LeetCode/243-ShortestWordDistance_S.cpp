c8d4f60fa61e62e9f5bf1288c702f4ea18c9addf
reiyo <47709174+reiyo@users.noreply.github.com>
2021-04-30 17:52:27 +0800

    Update 243-ShortestWordDistance_S.cpp

--- LeetCode/243-ShortestWordDistance_S.cpp
+++ LeetCode/243-ShortestWordDistance_S.cpp
@@ -1,17 +1,48 @@
+// time complexity: O(n * m), where n is the number of words and m is the sum of the lengths of word1 and word2
+// space complexity: O(1)
 class Solution {
 public:
-    int shortestDistance(vector<string>& words, string word1, string word2) {
-        int pos1 = -1;
-        int pos2 = -1;
-        int minDist = static_cast<int>(words.size());
-        
-        for (int i=0; i<static_cast<int>(words.size()); ++i) {
-            if (words[i] == word1) pos1 = i;
-            else if (words[i] == word2) pos2 = i;
-                
-            if (pos1 >=0 && pos2 >= 0) minDist = min(minDist, abs(pos1-pos2));
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        if (word1 == word2) return 0;
+        int minDist = wordsDict.size();
+        int id1 = -1;
+        int id2 = -1;
+
+        for (int i=0; i<wordsDict.size(); ++i) {
+            if (wordsDict[i] == word1) {
+                id1 = i;
+                if (id2 >= 0) minDist = min(minDist, id1-id2);
+            }
+            else if (wordsDict[i] == word2) {
+                id2 = i;
+                if (id1 >= 0) minDist = min(minDist, id2-id1);
+            }
         }
-        
+
+        return minDist;
+    }
+};
+
+// optimization:
+// 1. time: return 1 immediately if current min distance equals 1
+// 2. space: keep only one index
+class Solution {
+public:
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        if (word1 == word2) return 0;
+        int minDist = wordsDict.size();
+        int last_id = -1;
+
+        for (int i=0; i<wordsDict.size(); ++i) {
+            if (wordsDict[i] == word1 || wordsDict[i] == word2) {
+                if (last_id != -1 && wordsDict[last_id] != wordsDict[i]) {
+                    minDist = min(minDist, i-last_id);
+                    if (minDist == 1) return 1;
+                }
+                last_id = i;
+            }
+        }
+
         return minDist;
     }
 };
