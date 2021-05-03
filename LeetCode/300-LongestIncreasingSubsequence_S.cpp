b0ed65eede6da8638f8c13c4df23a42ed23bcda3
reiyo <47709174+reiyo@users.noreply.github.com>
2021-05-04 03:21:36 +0800

    Create 300-LongestIncreasingSubsequence_S.cpp

---
+++ LeetCode/300-LongestIncreasingSubsequence_S.cpp
@@ -0,0 +1,48 @@
+// time complexity: O(n^2), where n is the number of inputs
+// space complexity: O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // dp[i] stores the max length of LIS up to nums[i]
+        vector<int> dp(nums.size(), 1);
+
+        for (int i=1; i<nums.size(); ++i) {
+            for (int j=0; j<i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
+// time complexity: O(n log n), where n is the number of inputs
+// space complexity: O(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // tail[i] stores the LIS with the smallest end element among LIS's with length i+1
+        vector<int> tail(nums.size(), 0);
+        int size = 0;
+
+        for (int n: nums) {
+            int left = 0;
+            int right = size; // tail[size] has not been defined  
+
+            // find index so that tails[index-1] < x <= tails[index]
+            while (left < right) {
+                int mid = left + (right-left)/2;
+
+                if (tail[mid] < n) left = mid + 1;
+                else right = mid; // not mid-1
+            }
+
+            tail[left] = n;
+            if (left == size) ++ size;
+        }
+
+        return size;
+    }
+};
