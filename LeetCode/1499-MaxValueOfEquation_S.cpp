305ab77b085ea96aded1bb37c65301a1891495a5
reiyo <47709174+reiyo@users.noreply.github.com>
2021-05-23 01:05:28 +0800

    Create 1499-MaxValueOfEquation_S.cpp

---
+++ LeetCode/1499-MaxValueOfEquation_S.cpp
@@ -0,0 +1,34 @@
+// time complexity: O(n)
+// space complexity: O(n)
+// yi + yj + |xi - xj| = yi + yj + (xj - xi) = (yi - xi) + (yj + xj)
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        deque<int> dq;
+        int res = INT_MIN;
+
+        for (int i=0; i<points.size(); ++i) {
+            int x = points[i][0];
+            int y = points[i][1];
+
+            while (!dq.empty() && (x - points[dq.front()][0]) > k) {
+                dq.pop_front();
+            } 
+
+            if (!dq.empty()) {
+                res = max(res, points[dq.front()][1]-points[dq.front()][0]+x+y);
+            }
+
+            int diff = y - x;
+
+            // ensure the queue to be monotonically decreasing 
+            while (!dq.empty() && (points[dq.back()][1] - points[dq.back()][0]) < diff) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return res;
+    }
+};
