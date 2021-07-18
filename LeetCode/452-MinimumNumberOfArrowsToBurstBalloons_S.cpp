ad9dfd0fbf4e466eb164032136d920834692413d
reiyo <47709174+reiyo@users.noreply.github.com>
2021-07-19 02:25:28 +0800

    Update 452-MinimumNumberOfArrowsToBurstBalloons_S.cpp

--- LeetCode/452-MinimumNumberOfArrowsToBurstBalloons_S.cpp
+++ LeetCode/452-MinimumNumberOfArrowsToBurstBalloons_S.cpp
@@ -3,8 +3,14 @@
 // Space Complexity: O(1)
 // Sorting allows us to find the last position (the last chance) for the first arrow, so as to burst the first balloon
 // We will shoot at the ending position of the first balloon
-// if we shoot before the ending position and we can brust the same set of ballons, we can move the position to the ending position
-// if we shoot before the ending position and leave some overlapped balloons, we still need to burst the overlapped balloons later 
+// For the proof,
+// If we shoot before the ending position for a specific interval and we can brust the same set of balloons, we can always move the position to the ending position
+// Assume we can achieve a smaller number of arrows if we shoot before the ending position and leave some overlapped balloons
+// The left balloons will be either convered by existing arrows or additional arrow(s)
+// The total number of arrows is either the same or increased, and thus result contradicts our assumption
+// That is, we cannot achieve a samller number of arrows if we shoot before the ending position and leave some overlapped balloons
+// Remind that we can always move the position to the ending position if we can brust the same set of balloons
+// Therefore, the greedy algorithm below can obtain the minimum number of arrows
 class Solution {
 public:
     static bool cmp(const vector<int>& p1, const vector<int>& p2) {
